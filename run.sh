fb6beb3975b7eb44deb1857cd7d0ce33b9924883
Kristjan <kristjan.kivilaan@gmail.com>
2026-09-04 15:17:05 +0300

    docs: add gunicorn execution comments to run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-.venv/bin/gunicorn -b 127.0.0.1:5000 --reload app:app
 
+# .venv/bin/gunicorn -b 127.0.0.1:5000 --reload app:app
+# The first app is the filename (app.py)
+# The second app is the Flask object inside it (app = Flask(__name__))
+
+.venv/bin/gunicorn -b 127.0.0.1:5000 --reload app:app
